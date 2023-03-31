7b7d913d098237c53c723250c30546ac3c80eb60
alfandi <fandidixpc1@gmail.com>
2023-03-31 11:04:01 +0700

    Membuat input array

--- Quicksort_152/Quicksort_152.cpp
+++ Quicksort_152/Quicksort_152.cpp
@@ -5,3 +5,16 @@ using namespace std;
 int arr[20];
 int cmp_count = 0; // Number of comparision
 int mov_count = 0; // number of movement
+int n;
+
+void input() {
+	while (true) {
+		cout << "Masukkan panjang element array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\n Masukkan panjang array adalah 20"
+	}
+}
